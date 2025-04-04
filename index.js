10e731b556984ca1b13065cdcc56a2fdbb81fb88
Yash Nandha <92366452+Yashnandha@users.noreply.github.com>
2025-04-04 11:02:11 +0530

    fix issue

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
+
+import 'react-native-get-random-values';
+import 'moment-timezone';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Text, TextInput} from 'react-native';
 
 AppRegistry.registerComponent(appName, () => App);
-// For Disabled Fontscaling from System when Bundle load
+
 if (Text.defaultProps == null) {
-    Text.defaultProps = {};
-    Text.defaultProps.allowFontScaling = false;
-  }
-  
-  if (TextInput.defaultProps == null) {
-    TextInput.defaultProps = {};
-    TextInput.defaultProps.allowFontScaling = false;
-  }
+  Text.defaultProps = {};
+  Text.defaultProps.allowFontScaling = false;
+}
+
+if (TextInput.defaultProps == null) {
+  TextInput.defaultProps = {};
+  TextInput.defaultProps.allowFontScaling = false;
+}
